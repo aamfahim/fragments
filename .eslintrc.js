432a9d79e7b39d456e2045b208e076aac32838f2
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-09-21 13:07:04 -0400

    added jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
     "env": {
         "commonjs": true,
         "es2021": true,
-        "node": true
+        "node": true,
+         // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+        "jest": true,
     },
     "extends": "eslint:recommended",
     "overrides": [
