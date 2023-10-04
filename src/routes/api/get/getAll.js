e0e62bf7d5026bb23177304ae7e9079aaf7704e9
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-10-04 18:12:47 -0400

    fixed logger.error

--- src/routes/api/get/getAll.js
+++ src/routes/api/get/getAll.js
@@ -13,9 +13,9 @@ module.exports = async (req, res) => {
   const expand = (req?.query?.expand == 1 ? true : false) || false;
   try {
     const data = await Fragment.byUser(req.user, expand);
-    
+
     util.setHeader(req, res);
-    
+
     // const baseUrl = 'http://' + req.headers.host + '/v1/fragments';
     // res.setHeader('Location', baseUrl);
     // res.setHeader('Access-Control-Expose-Headers', 'Location');
@@ -23,7 +23,7 @@ module.exports = async (req, res) => {
     const response = createSuccessResponse(data);
     res.status(200).json(response);
   } catch (error) {
-    logger.error({ error });
+    logger.error(error);
     const response = createErrorResponse(400, { error });
     res.status(response.error.code).json(response);
   }
