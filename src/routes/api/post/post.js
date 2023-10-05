d9d5bf46d3fcab55306094e2a6818d2bb2d00b4b
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-10-05 17:56:15 -0400

    refactored

--- src/routes/api/post/post.js
+++ src/routes/api/post/post.js
@@ -7,21 +7,26 @@ const util = require("../../../util");
 module.exports = async (req, res) => {
 
     try {
-        const newFragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type'), size: req.body.byteLength });
-        
-        logger.info({ newFragment }, 'created');
-        
-        await newFragment.setData(req.body);
-        util.setHeader(req, res, newFragment);
+        if (Buffer.isBuffer(req.body)) {
+            const newFragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type'), size: req.body.byteLength });
 
-        const response = createSuccessResponse(newFragment);
+            logger.info({ newFragment }, 'created');
 
-        res.status(200).json(response);
+            await newFragment.setData(req.body);
+            util.setHeader(req, res, newFragment);
+
+            const response = createSuccessResponse(newFragment);
+
+            return res.status(201).json(response);
+        } else {
+            const response = createErrorResponse(415, 'Unsupported type');
+            return res.status(response.error.code).json(response);
+        }
 
     } catch (error) {
         logger.error(error);
-        const response = createErrorResponse(400, { error });
-        res.status(response.error.code).json(response);
+        const response = createErrorResponse(400, error);
+        return res.status(response.error.code).json(response);
     }
 
 }
