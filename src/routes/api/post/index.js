5254a7b16e8151cf5c30a8bdd9361fb959d74601
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-10-07 11:17:09 -0400

    added comments

--- src/routes/api/post/index.js
+++ src/routes/api/post/index.js
@@ -1,9 +1,19 @@
+// src/routes/api/post/index.js
+
 const Express = require('express');
+
+// Create a router on which to mount our API endpoints
 const router = Express.Router();
 
+// import the defined functions
 const post = require("./post");
 
-// post a Fragment object for a user with buffer from body
+/**
+ * POST /v1/fragments/
+ * Post a Fragment object for the user defined in the request with buffer from body
+ */
 router.post("/", post);
+
+// Other routes will go here later on...
 
 module.exports = router;
