2365643e10a31e2f33f9b51b4cb8b4c256b9b58e
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-10-07 09:18:33 -0400

    added comments defining routes

--- src/routes/api/get/index.js
+++ src/routes/api/get/index.js
@@ -1,16 +1,29 @@
+// src/routes/api/get/index.js
+
 const Express = require('express');
+
+// Create a router on which to mount our API endpoints
 const router = Express.Router();
 
-
+// import the defined functions
 const getAll = require("./getAll");
 const getById = require("./getById");
 
-// get all Fragment for a user
+/**
+ * Define our first route, which will be: 
+ * GET /v1/fragments
+ * Will get all the fragments from the user defined in the request
+*/
 router.get("/", getAll);
 
-// get Fragment by id for a user
-router.get("/:id.:ext?", getById); // optional ext for id
+/**
+ * GET /v1/fragments/:id.:ext?
+ * Will get Fragment by id from the user defined in the request
+ * Optional ext for id to get fragments by ext
+ */
+router.get("/:id.:ext?", getById); 
 
+// Other routes will go here later on...
 
 
 module.exports = router;
