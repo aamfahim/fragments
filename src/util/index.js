73243489cd977c4ce822e852164678b4d64b8e8a
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-10-07 11:32:43 -0400

    added logger and commments

--- src/util/index.js
+++ src/util/index.js
@@ -1,14 +1,21 @@
 const logger = require("../logger");
 
 module.exports.setHeader = (req, res, fragment) => {
-    logger.info('modifying req and res in setHeader');
-    const baseUrl = 'http://' + req.headers.host + '/v1/fragments/';
-    res.setHeader('Access-Control-Expose-Headers', 'Location');
-    if (fragment) {
-        res.setHeader('Location', baseUrl + fragment.id);
-        res.setHeader('Content-Type', fragment.mimeType);
-    } else {
-        res.setHeader('Location', baseUrl);
+    logger.info('modifying response header in setHeader');
 
+    logger.debug(res.headers, "before set");
+
+    const baseUrl = 'http://' + req.headers.host + '/v1/fragments/'; // create base url
+
+    res.setHeader('Access-Control-Expose-Headers', 'Location'); // add Location to Access-Control-Expose-Headers
+
+    if (fragment) {
+        res.setHeader('Location', baseUrl + fragment.id); // if fragment provided set location with fragment id
+        res.setHeader('Content-Type', fragment.mimeType); // also set the content type according to fragment's
+    } else {
+        res.setHeader('Location', baseUrl); // just set the default url
     }
+
+    logger.debug(res.headers, "after set");
+
 }
