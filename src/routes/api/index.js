b83a38ece0e9542028b6d78ef4d9d997fae9c46c
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-10-07 09:13:23 -0400

    added comments on api/index.js

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -8,9 +8,13 @@ const express = require('express');
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
+// Use https://www.npmjs.com/package/content-type to create/parse Content-Type headers
 const contentType = require('content-type');
+
+// Use Fragment model
 const { Fragment } = require('../../model/fragment');
 
+// Get all the defined get and post routes
 const get = require("./get");
 const post = require("./post");
 
@@ -32,13 +36,13 @@ const rawBody = () =>
 
 
 
-// Define our first route, which will be: GET /v1/fragments
+// Attach all our get routes
 router.use('/fragments', get);
 
+// Attach all our post routes
 // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
 // You can use Buffer.isBuffer(req.body) to test if it was parsed by the raw body parser.
 router.use('/fragments', rawBody(), post);
 
-// Other routes will go here later on...
 
 module.exports = router;
