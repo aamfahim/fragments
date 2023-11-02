3b423b00dc53da443b5d037ed9122dadb8d08749
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-11-02 16:52:06 -0400

    added text/html text/markdown and converstion respectively

--- src/routes/api/get/getById.js
+++ src/routes/api/get/getById.js
@@ -4,6 +4,8 @@ const logger = require("../../../logger");
 const { Fragment } = require("../../../model/fragment");
 const util = require("../../../util");
 const { createErrorResponse } = require("../../../response");
+const md = require('markdown-it')();
+
 
 /**
  * Get a fragment for the current user by id
@@ -20,24 +22,75 @@ module.exports = async (req, res) => {
         util.setHeader(req, res, fragment);
 
         // if no extension is provided
-        if (!ext) {                 
+        if (!ext) {
             if (fragment.isText) {  // and if the fragment is text
                 return res.status(200).send(data);
             }
             else if (fragment.mimeType == "application/json") { // and if the fragment is json
-                return res.status(200).json(data);
+                const JsonData = JSON.parse(data);
+                return res.status(200).json(JsonData);
             }
             else if (fragment.mimeType.startsWith("image/")) { // and if the fragment is image
                 //TODO: make adjustments for image
                 return res.status(200).send(data);
             }
         }
-        // Check if an extension is provided and it is text/plain
-        else if (ext == "txt" && fragment.mimeType == "text/plain") {
-            return res.status(200).send(data);
+        // if the extension is supported
+        else if (fragment.formats.includes(ext)) {
+            logger.info(fragment.formats.includes(ext), 'ext support');
+
+            // make conversion
+
+            // if text no conversion
+            if (fragment.mimeType == "text/plain") {
+                return res.status(200).send(data);
+            }
+
+            // if html convert to text, html
+            if (fragment.mimeType == "text/html") {
+                if (ext == "txt") {
+                    util.setHeader(req, res, fragment, "text/plain");
+                }
+                else if (ext == "html") {
+                    util.setHeader(req, res, fragment);
+                }
+                return res.status(200).send(data);
+            }
+
+            // if json convert to json, text
+            if (fragment.mimeType == "application/json") {
+                if (ext == "json") {
+                    const JsonData = JSON.parse(data);
+                    return res.status(200).json(JsonData);
+                }
+                else if (ext == "txt") {
+                    const JsonData = JSON.parse(data);
+                    util.setHeader(req, res, fragment, "text/plain");
+                    return res.status(200).json(JsonData);
+                }
+            }
+
+            // if md convert to text, html, md
+            if (fragment.mimeType == "text/markdown") {
+                if (ext == "txt") {
+                    util.setHeader(req, res, fragment, "text/plain");
+                    return res.status(200).send(data);
+
+                }
+                else if (ext == "html") {
+                    util.setHeader(req, res, fragment, "text/html");
+                    const result = md.render(data); // convert to html
+                    return res.status(200).send(result);
+                }
+                else if (ext == "md") {
+                    util.setHeader(req, res, fragment);
+                    return res.status(200).send(data);
+                }
+            }
+            // send appropriate response
 
         }
-        else {            
+        else {
             const response = createErrorResponse(415, 'Unsupported media type or conversion not possible');
             return res.status(response.error.code).send(response);
         }
