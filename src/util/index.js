4dbc119ce3852f5b85a4f251ee98427d4b5c53a3
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-11-02 16:45:06 -0400

    added custom logic for adding content type

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,6 @@
 const logger = require("../logger");
 
-module.exports.setHeader = (req, res, fragment) => {
+module.exports.setHeader = (req, res, fragment, type = null) => {
     logger.info('modifying response header in setHeader');
 
     logger.debug(res.headers, "before set");
@@ -16,9 +16,21 @@ module.exports.setHeader = (req, res, fragment) => {
 
     res.setHeader('Access-Control-Expose-Headers', 'Location'); // add Location to Access-Control-Expose-Headers
 
+
+
     if (fragment) {
         res.setHeader('Location', baseUrl + fragment.id); // if fragment provided set location with fragment id
         res.setHeader('Content-Type', fragment.mimeType); // also set the content type according to fragment's
+
+        if (type) {
+            const contentType = res.getHeader('Content-Type');
+            const parts = contentType.split(';').map(part => part.trim()); // extract the media type from the content type
+            const newMediaType = type; // Set your new media type here
+            parts[0] = newMediaType;
+            
+            const newContentType = parts.join(';');
+            res.setHeader('Content-Type', newContentType);
+        }
     } else {
         res.setHeader('Location', baseUrl); // just set the default url
     }
