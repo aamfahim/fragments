0bf8b193d3709e916d25f8a5281b4265431869f7
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-11-20 23:49:01 -0500

    added unit test for delete route

---
+++ tests/unit/delete.test.js
@@ -0,0 +1,78 @@
+// tests/unit/delete.test.js
+
+const request = require('supertest');
+
+let app = require('../../src/app');
+
+describe('DELETE /v1/fragments/:id', () => {
+
+    beforeEach(() => {
+        // Re-import module to get a fresh state:
+        jest.resetModules();
+        app = require('../../src/app');
+    });
+
+    // Unauthorized access is denied
+    test('unauthorized access is denied', async () => {
+        const res = await request(app)
+            .delete('/v1/fragments/valid-fragment-id');
+
+        expect(res.statusCode).toBe(401);
+    });
+
+    // Authenticated user can delete a fragment
+    test('authenticated users can delete a fragment', async () => {
+        const postRes = await request(app)
+            .post('/v1/fragments/')
+            .auth('user1@email.com', 'password1')
+            .set('Content-Type', 'text/plain')
+            .send('this is a fragment');
+
+        expect(postRes.statusCode).toBe(201); // post a fragment
+        const fragmentId = postRes.body.fragment.id; // get id from response
+
+        const delRes = await request(app)
+            .delete(`/v1/fragments/${fragmentId}`)
+            .auth('user1@email.com', 'password1');
+
+        expect(delRes.statusCode).toBe(200); // check status code
+    });
+
+    // Verify response structure
+    test('response structure is correct after deletion', async () => {
+        const postRes = await request(app)
+            .post('/v1/fragments/')
+            .auth('user1@email.com', 'password1')
+            .set('Content-Type', 'text/plain')
+            .send('this is a fragment');
+
+        expect(postRes.statusCode).toBe(201); // post a fragment
+        const fragmentId = postRes.body.fragment.id; // get id from response
+
+        const delRes = await request(app)
+            .delete(`/v1/fragments/${fragmentId}`)
+            .auth('user1@email.com', 'password1');
+
+        expect(delRes.body).toMatchObject({ status: 'ok' }); // match obeject structure
+    });
+
+    // Non-existent fragment ID returns a 404
+    test('non-existent fragment ID returns a 404', async () => {
+        const postRes = await request(app)
+            .post('/v1/fragments/')
+            .auth('user1@email.com', 'password1')
+            .set('Content-Type', 'text/plain')
+            .send('this is a fragment');
+
+        expect(postRes.statusCode).toBe(201); // post a fragment
+        const fragmentId = postRes.body.fragment.id; // get id from response
+
+        const delRes = await request(app)
+            .delete(`/v1/fragments/${fragmentId+1}`)
+            .auth('user1@email.com', 'password1');
+
+        expect(delRes.statusCode).toBe(404);
+    });
+
+
+});
